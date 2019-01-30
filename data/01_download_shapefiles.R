4a1c908ea97f427a23323a091dcdf9aba74c3426
Declan Molloy <dmolloy3@gmail.com>
2019-01-29 19:45:52 -0500

    initial shapefile build

--- data/01_download_shapefiles.R
+++ data/01_download_shapefiles.R
@@ -1,8 +1,11 @@
-# Scrape the AEC website for shapefiles
+# Scrape the AEC website for shapefiles, then build and export them
 
 library(tidyverse)
+library(sf)
 
-# Get file paths ----------------------------------------------------------
+
+# Scrape ------------------------------------------------------------------
+# Get file paths
 # Set website to download from
 url <- "https://www.aec.gov.au/electorates/gis/gis_datadownload.htm"
 
@@ -32,16 +35,60 @@ download.file(
   cacheOK = FALSE
 )
 
-# Unzip -------------------------------------------------------------------
+# Unzip
 unzip_dest <- str_replace(file_dest, ".zip$", "")
-# walk(unzip_dest, unlink)
 pwalk(list(zipfile = file_dest, exdir = unzip_dest), unzip)
 
-# Delete zips -------------------------------------------------------------
+# Delete zips
 walk(file_dest, file.remove)
 
-# Zip whole thing ---------------------------------------------------------
-zip(here::here("data", "shapefiles.zip"), unzip_dest)
+# Load shapefiles ---------------------------------------------------------
+# List of shapefiles
+shp_files <- list.files(recursive = T) %>% 
+  str_subset(".shp$") %>% 
+  str_subset("SA1s", negate = T) # Needs dev version of stringr
+
+# List of states
+states <- shp_files %>% str_sub(6, 8) %>% 
+  str_remove("-") %>% 
+  str_to_upper()
+
+# Get a list of shapefiles
+state_shp_list <- shp_files %>% 
+  map(read_sf) %>% 
+  # Name the elements of the list
+  set_names(states)
+
+# Wrangle -----------------------------------------------------------------
+# Transform each State's CRS so that they are all the same
+for (i in seq_along(state_shp_list)) {
+  state_shp_list[[i]] <- state_shp_list[[i]] %>% st_transform(3577)
+}
+
+# Collapse the list into a normal df
+electorates <- sf::st_as_sf(data.table::rbindlist(state_shp_list, idcol = TRUE)) %>% 
+  rename(state = .id)
+
+# Export and clean up -----------------------------------------------------
+write_sf(electorates, here::here("data", "electorates.gpkg"))
 
 # Delete folders
 walk(unzip_dest, unlink, recursive = T) 
+
+# Make sure it worked -----------------------------------------------------
+
+test <- read_sf(here::here("data", "electorates.gpkg")) %>% rename(geometry = geom)
+
+p1 <- ggplot(test) + 
+  geom_sf() + 
+  theme_void() + 
+  theme_minimal()
+
+ggsave(here::here("static", "last_map.pdf"), p1)
+
+
+# To do
+# Go back and do Nat data as they want me to
+# Prettify map
+
+
